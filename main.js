8cacf6b6d88209bae86c88f114b77b4198b522c3
Jeremy <jcbball55@gmail.com>
2022-01-15 13:11:06 -0500

    Complete redesign

--- main.js
+++ main.js
@@ -1,99 +1,49 @@
-document.body.classList.add("js-loading");
+function skillsLeftClicked() {
+    document.querySelector(".primary").classList.add("p2l");
+    document.querySelector(".back").classList.add("b2r");
+    document.querySelector(".left").classList.add("l2b");
+    document.querySelector(".right").classList.add("r2p");
 
-window.addEventListener("load", showPage);
+    document
+        .querySelector(".primary")
+        .addEventListener("animationend", function () {
+            console.log("function called");
 
-function showPage() {
-    console.log("loaded");
-    document.body.classList.remove("js-loading");
-    document.querySelector(".Header").style.backgroundImage =
-        "url(images/peter-gargiulo-cGNCepznaV8-unsplash.jpg)";
+            document.querySelector(".p2l").classList.add("left");
+            document.querySelector(".p2l").classList.remove("primary", "p2l");
+
+            document.querySelector(".b2r").classList.add("right");
+            document.querySelector(".b2r").classList.remove("back", "b2r");
+
+            document.querySelector(".l2b").classList.add("back");
+            document.querySelector(".l2b").classList.remove("left", "l2b");
+
+            document.querySelector(".r2p").classList.add("primary");
+            document.querySelector(".r2p").classList.remove("right", "r2p");
+        });
 }
 
-function homeClicked() {
-    location.href = "index.html";
-}
+function skillsRightClicked() {
+    document.querySelector(".primary").classList.add("p2r");
+    document.querySelector(".back").classList.add("b2l");
+    document.querySelector(".left").classList.add("l2p");
+    document.querySelector(".right").classList.add("r2b");
 
-function aboutClicked() {
-    location.href = "about.html";
-}
+    document
+        .querySelector(".primary")
+        .addEventListener("animationend", function () {
+            console.log("function called");
 
-function portfolioClicked() {
-    location.href = "portfolio.html";
-}
+            document.querySelector(".p2r").classList.add("right");
+            document.querySelector(".p2r").classList.remove("primary", "p2r");
 
-function contactClicked() {
-    location.href = "contact.html";
-}
+            document.querySelector(".b2l").classList.add("left");
+            document.querySelector(".b2l").classList.remove("back", "b2l");
 
-function toggleMobileMenu() {
-    document.querySelector(".Mobile-Menu").classList.toggle("active");
-}
+            document.querySelector(".l2p").classList.add("primary");
+            document.querySelector(".l2p").classList.remove("left", "l2p");
 
-function arrowClicked() {
-    let viewHeight = window.innerHeight;
-    console.log("Arrow");
-    window.scrollBy({
-        top: viewHeight, // could be negative value
-        left: 0,
-        behavior: "smooth",
-    });
-}
-
-function upArrowClicked() {
-    let viewHeight = window.innerHeight;
-    console.log("Arrow");
-    window.scrollBy({
-        top: -viewHeight, // could be negative value
-        left: 0,
-        behavior: "smooth",
-    });
-}
-
-//Deal with carousel
-function rightArrowClicked() {
-    let pages = document.querySelectorAll(".Page");
-
-    let activePage;
-    for (let i = 0; i < pages.length; i++) {
-        if (getComputedStyle(pages[i]).display == "flex") {
-            activePage = i;
-            console.log(activePage);
-        }
-    }
-
-    if (activePage == pages.length - 1) {
-        pages[activePage].style.display = "none";
-        pages[0].style.display = "flex";
-    } else {
-        pages[activePage].style.display = "none";
-        pages[activePage + 1].style.display = "flex";
-    }
-}
-
-function leftArrowClicked() {
-    let pages = document.querySelectorAll(".Page");
-
-    let activePage;
-    for (let i = 0; i < pages.length; i++) {
-        if (getComputedStyle(pages[i]).display == "flex") {
-            activePage = i;
-            console.log(activePage);
-        }
-    }
-
-    if (activePage == 0) {
-        pages[activePage].style.display = "none";
-        pages[pages.length - 1].style.display = "flex";
-    } else {
-        pages[activePage].style.display = "none";
-        pages[activePage - 1].style.display = "flex";
-    }
-}
-
-function activateQuote() {
-    let initQuote = document.getElementById("Shown");
-    let betterQuote = document.getElementById("Hidden");
-
-    initQuote.style.animationPlayState = "running";
-    betterQuote.style.animationPlayState = "running";
+            document.querySelector(".r2b").classList.add("back");
+            document.querySelector(".r2b").classList.remove("right", "r2b");
+        });
 }
