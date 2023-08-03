53f1cbaa3ad999aa9b8b102a893d3ec07f19a578
Jeremy Cavallo <jjcavallo@wayne.edu>
2023-08-03 14:46:32 -0400

    Fixed bug with days off by 1

--- github.js
+++ github.js
@@ -25,13 +25,38 @@ const COLORS = [
     '#800080ff'
 ]
 
+const DAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+]
+
+const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
 const generateGithubContributionGraph = (contributions_json) => {
     let contributions = contributions_json['data']['user']['contributionsCollection']['contributionCalendar']['weeks']
-    console.log(contributions)
     let totalContributions = contributions_json['data']['user']['contributionsCollection']['contributionCalendar']['totalContributions']
 
     let container = document.getElementsByClassName('github-graph-container')[0]
-    console.log(container)
+    let contributionsElement = document.getElementById('total-contributions')
+    contributionsElement.innerHTML = `Total Contributions: ${totalContributions}`
 
     let maxDailyContribution = 0;
     for (let i = 0; i < contributions.length; i++) {
@@ -46,6 +71,7 @@ const generateGithubContributionGraph = (contributions_json) => {
         for (let j = 0; j < contributions[i]['contributionDays'].length; j++) {
             let contributionNumber = contributions[i]['contributionDays'][j]['contributionCount']
             let contributionDate = new Date(contributions[i]['contributionDays'][j]['date'])
+            // console.log(contributions[i]['contributionDays'][j]['date'])
             let color = '#2d333b'
             if (contributionNumber > 0) color = COLORS[0]
             if (contributionNumber > maxDailyContribution / 4) color = COLORS[1]
@@ -55,7 +81,7 @@ const generateGithubContributionGraph = (contributions_json) => {
             let tooltip = document.createElement('p')
             block.classList.add('github-day-block')
             tooltip.classList.add('github-tooltip')
-            tooltip.innerHTML = `${contributionNumber} contribution${contributionNumber == 1 ? '' : 's'} on ${contributionDate.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}`
+            tooltip.innerHTML = `${contributionNumber} contribution${contributionNumber == 1 ? '' : 's'} on ${DAYS[contributionDate.getUTCDay()]}, ${MONTHS[contributionDate.getUTCMonth()]} ${contributionDate.getUTCDate()}, ${contributionDate.getUTCFullYear()}`
             // tooltip.style = 'display: none';
             block.appendChild(tooltip)
             block.style = `height: 10px; width: 10px; background-color: ${color}; margin: 2px;`
